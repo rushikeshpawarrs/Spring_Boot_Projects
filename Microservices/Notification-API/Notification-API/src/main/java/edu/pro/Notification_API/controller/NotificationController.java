564ef14be8683f126958a8f8b844ead583e04749
Rushikesh Pawar <rushikeshpawarrs@gmail.com>
2026-09-11 17:48:13 +0530

    FeignClient added

--- Microservices/Notification-API/Notification-API/src/main/java/edu/pro/Notification_API/controller/NotificationController.java
+++ Microservices/Notification-API/Notification-API/src/main/java/edu/pro/Notification_API/controller/NotificationController.java
@@ -1,13 +1,26 @@
 package edu.pro.Notification_API.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NotificationController {
 
+	@Autowired
+	private ProductAPI productAPI;
+	
+	
 	@GetMapping("/notification")
 	public String notification() {
 		return "Email Sent to user";
 	}
+	
+	@GetMapping("/notification/product")
+	public String getProduct() {
+		String s2 = productAPI.invokeProductAPI();
+		return s2;
+	}
+	
+	
 }
